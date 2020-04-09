ecf107339c5cc0896c45dc23ed6d1bd4d111bafd
dmytro.malkin <malkin.dmytro@gmail.com>
2020-04-09 17:49:27 +0300

    minor

--- run.sh
+++ run.sh
@@ -14,16 +14,13 @@ docker-compose up -d
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:4000)" != "302" ]]; do echo 'Waiting grafana...'; sleep 1; done
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9086)" != "404" ]]; do echo 'Waiting influxdb...'; sleep 1; done
 
-echo
 curl -XPOST 'http://localhost:9086/query' --data-urlencode 'q=CREATE DATABASE temp_db'
-echo '\n'
+echo -e '\n'
 
-echo
 curl -XPOST -u admin:admin  http://localhost:4000/api/datasources -H "content-type: application/json"  -d @./datasource.json
-echo '\n'
+echo -e '\n'
 
-echo
 curl -XPOST -u admin:admin  http://localhost:4000/api/dashboards/db -H "content-type: application/json"  -d @./dashboard.json
-echo '\n'
+echo -e '\n'
 
 python3 influx.py $1
